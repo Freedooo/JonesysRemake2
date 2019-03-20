f26ac7294175bc888371d89126b5a97b2c32cc2c
Matt <mattfreedy@gmail.com>
2019-03-20 17:16:21 -0500

    Created Register and Register Error/Success message

---
+++ jonesysregister.php
@@ -0,0 +1,56 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8" />
+	<title>Jonesy's Resturaunt + Lounge | Reservation</title>
+	<link rel="stylesheet" type="text/css" href="jonesysreservation.css">
+	<script src="jonesys.js" type="text/javascript"></script>
+</head>
+<body>
+	<div id ="heading">
+		<img src="friday.png" id="friday" alt="friday">
+		<img src="jonesystitle.png" id="titlePic" alt="title">
+		<img src="saturday.png" id="saturday" alt="saturday">
+	</div>
+
+	<nav id = "headingNav">
+		<ul>
+			<li><a href="index.html">Home</a></li>
+			<li><a href="jonesysmenu.php">Menu</a></li>
+			<li><a href="jonesysreservation.php">Reservation</a></li>
+			<li><a href="jonesysreviews.php">Reviews</a></li>
+			<li><a href="jonesysregister.php">Register</a></li>
+			<li><a href="jonesyslogin.php">Log in</a></li>
+			<li><a href="jonesyslogout.php">Log out</a></li>
+		</ul>
+    </nav>
+    
+    <h3>Register for an account</h3>
+    <div id = 'register'>
+        <form action="process_post.php" method ='POST'>
+            <label for="email">Enter an Email:</label>
+            <input type="email" name="email" required>
+            
+            <label for="password">Enter a Password:</label>
+            <input type="password" name="password" required>
+
+            <label for="repassword">Re-enter password:</label>
+            <input type="password" name="repassword" required>
+
+            <button type="submit" id="submit" name ='command' value ='Register'>Create Account</button>
+
+        </form>
+    </div>
+
+	<footer>
+		<nav id = "footerNav">
+			<ul>
+				<li><a href="index.html">Home</a></li>
+				<li><a href="jonesysmenu.html">Menu</a></li>
+				<li><a href="jonesysreservation.html">Reservation</a></li>
+			</ul>
+		</nav>
+	</footer>
+</body>
+
+</html>
--- process_post.php
+++ process_post.php
@@ -38,7 +38,7 @@
         $comments = filter_input(INPUT_POST, 'comments', FILTER_SANITIZE_FULL_SPECIAL_CHARS);
         $id = filter_input(INPUT_POST, 'id', FILTER_SANITIZE_NUMBER_INT);
         
-        var_dump("reached");
+        
         $updateQuery = "UPDATE reservation SET date = :date, fullname = :fullname,phone = :phone,email = :email,section = :section,comments = :comments WHERE id = :id";
         $statement = $db->prepare($updateQuery);
         $statement->bindValue(':date', $date);  
@@ -63,4 +63,74 @@
         header('Location: viewreservation.php');
         exit();
     }
+
+    if($_POST['command'] =='Register')
+    {
+        $error = false;
+
+        $email = filter_input(INPUT_POST, 'email', FILTER_SANITIZE_EMAIL);
+        $password = filter_input(INPUT_POST, 'password', FILTER_SANITIZE_FULL_SPECIAL_CHARS);
+        $repassword = filter_input(INPUT_POST, 'repassword', FILTER_SANITIZE_FULL_SPECIAL_CHARS);
+        if($password == $repassword)
+        {
+            $insertQuery = "INSERT INTO users (email,password) values (:email,:password)";
+            $statement = $db->prepare($insertQuery);
+            $statement->bindValue(':email', $email);  
+            $statement->bindValue(':password', $password);  
+            $statement->execute(); 
+        }
+        else
+        {
+            $error = true;
+        }
+    }
 ?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8" />
+	<title>Jonesy's Resturaunt + Lounge | Reservation</title>
+	<link rel="stylesheet" type="text/css" href="jonesysreservation.css">
+	<script src="jonesys.js" type="text/javascript"></script>
+</head>
+<body>
+	<div id ="heading">
+		<img src="friday.png" id="friday" alt="friday">
+		<img src="jonesystitle.png" id="titlePic" alt="title">
+		<img src="saturday.png" id="saturday" alt="saturday">
+	</div>
+
+	<nav id = "headingNav">
+		<ul>
+			<li><a href="index.html">Home</a></li>
+			<li><a href="jonesysmenu.php">Menu</a></li>
+			<li><a href="jonesysreservation.php">Reservation</a></li>
+			<li><a href="jonesysreviews.php">Reviews</a></li>
+			<li><a href="jonesysregister.php">Register</a></li>
+			<li><a href="jonesyslogin.php">Log in</a></li>
+			<li><a href="jonesyslogout.php">Log out</a></li>
+		</ul>
+    </nav>
+    <?php if($error): ?>
+    <div>
+        <h3>Your passwords dont match, please <a href="jonesysregister.php"> try again</a></h3>
+    </div>
+    <?php else:?>
+    <div>
+        <h3>Account created successfully</h3>
+    </div>
+    <?php endif ?>
+	<footer>
+		<nav id = "footerNav">
+			<ul>
+				<li><a href="index.html">Home</a></li>
+				<li><a href="jonesysmenu.html">Menu</a></li>
+				<li><a href="jonesysreservation.html">Reservation</a></li>
+			</ul>
+		</nav>
+	</footer>
+</body>
+
+</html>
